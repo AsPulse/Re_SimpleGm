b0af80d5b0b04ab9f3f2edf9b63ab613e6e67d88
AsPulse <aspulse.x@gmail.com>
2021-05-15 21:50:45 +0900

    プロジェクトを初期化

---
+++ src/main/java/dev/aspulse/re_simplegm/Re_SimpleGm.java
@@ -0,0 +1,17 @@
+package dev.aspulse.re_simplegm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Re_SimpleGm extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
